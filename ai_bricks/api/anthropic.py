8a1ac204e253207de1affafd0013804a1e81bc62
Maciej Obarski <mobarski@gmail.com>
2023-03-20 22:34:03 +0100

    anthropic

---
+++ ai_bricks/api/anthropic.py
@@ -0,0 +1,44 @@
+import anthropic
+import time
+import os
+
+api_key = None
+
+def use_key(key):
+	global api_key
+	api_key = key
+if not api_key:
+	use_key(os.getenv('ANTHROPIC_KEY'))
+
+# TODO: ChatModel messages [('system','xxx'),('user','yyy'),('system','zzz'),('user','aaa')]
+class TextModel:
+	PARAMS = ['model','temperature','stop_sequences','max_tokens_to_sample']
+	
+	def __init__(self, name, **kwargs):
+		self.config = kwargs
+		self.config['model'] = name
+		self.client = anthropic.Client(api_key)
+	
+	def complete(self, prompt):
+		out = {}
+		#
+		kwargs = dict(
+			prompt = f"{anthropic.HUMAN_PROMPT} {prompt}{anthropic.AI_PROMPT}",
+			max_tokens_to_sample = 1000, # TODO
+		)
+		for k,v in self.config.items():
+			if k in self.PARAMS:
+				kwargs[k] = v
+		t0 = time.time()
+		#
+		resp = self.client.completion(**kwargs)
+		#
+		out['rtt'] = time.time() - t0
+		out['text'] = resp['completion']
+		out['raw'] = resp # XXX
+		return out
+
+
+# models: 
+def model(name, **kwargs):
+	return TextModel(name, **kwargs)
