55189845a11425aab3da4a779f5f1e7267f1fc88
Maciej Obarski <mobarski@gmail.com>
2023-03-21 00:08:06 +0100

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name='ai_bricks',
-	version='0.0.6',
+	version='0.0.7',
 	description='AI adapter / facade',
 	author='Maciej Obarski',
 	install_requires=[
