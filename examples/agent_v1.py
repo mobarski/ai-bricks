28fd76cf768481807b4a273b950256c70dcb1259
Maciej Obarski <mobarski@gmail.com>
2023-03-24 23:20:59 +0100

    agent cleanup

--- examples/agent_v1.py
+++ examples/agent_v1.py
@@ -6,30 +6,37 @@ from ai_bricks.api import anthropic
 import datetime
 
 # model = openai.model('text-davinci-003', temperature=0) # key from OPENAI_KEY env variable
-model = openai.model('gpt-3.5-turbo', temperature=0) # key from OPENAI_KEY env variable
+model = openai.model('gpt-3.5-turbo', temperature=0.5) # key from OPENAI_KEY env variable
 # model = openai.model('gpt-4', temperature=0) # key from OPENAI_KEY env variable
 # model = anthropic.model('claude-instant-v1', temperature=0.5) # key from ANTHROPIC_KEY env variable
-# model = anthropic.model('claude-v1', temperature=0.5) # key from ANTHROPIC_KEY env variable
+#model = anthropic.model('claude-v1', temperature=0.5) # key from ANTHROPIC_KEY env variable
 # model = anthropic.model('claude-v1.2', temperature=0.5) # key from ANTHROPIC_KEY env variable
 
+def log_state(kw, self):
+    print('PROMPT', kw['messages'][1]['content'])
+model.add_callback('before', log_state)
+
+agent_actions.VERBOSE_EXCEPTIONS = False
 actions = {
-	'[wikipedia-summary]': agent_actions.wikipedia_summary,
-	'[wikipedia-search]':  agent_actions.wikipedia_search,
-	'[python-eval]':       agent_actions.python_eval,
+	'wikipedia-page-summary': agent_actions.wikipedia_summary,
+	'wikipedia-find-pages':   agent_actions.wikipedia_search,
+	'python-eval':            agent_actions.python_eval,
+    'url-get-headlines':	  agent_actions.requests_get_headlines,
 }
 
 hints = f"""
-	You are augmented with the following actions: {' '.join(actions)}.
-	Action names are always surrounded by squere brackets: [example-action-name].
-	Following modules are already imported in [python-eval]: math, time, datetime, random.
-	Remember that [python-eval] only evaluetes expressions (up to 300 chars) and not code blocks!
-	Don't import any module on your own in [python-eval].
-	Current time: {datetime.datetime.today()}
+You are augmented with the following actions: {' '.join(actions)}.
+Following modules are already imported in *python-eval*: math, time, datetime, random.
+Remember that *python-eval* only evaluetes expressions (up to 300 chars) and not code blocks!
+Don't import any module on your own in *python-eval*.
+Be careful and avoid off by one errors.
+Current time: {datetime.datetime.today()}
 """
 
 question = "What is the sum of square root of every third number from 123 to the year of the recent Russian invasion of Ukraine?"
+#question = "Whats on the news today? Check tvn24."
 
-resp = agent.v2(question, model=model, actions=actions, hints=hints, iter_limit=10)
+resp = agent.v1(question, model=model, actions=actions, hints=hints, iter_limit=10)
 print('agent response:', resp)
 rtt_sum = sum(resp["rtt_list"])
 cost_sum = sum(resp.get('cost_list',[]))
