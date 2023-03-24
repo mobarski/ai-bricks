64553836336317b2ae77ad09e6d1b8e5bd5bd3f0
Maciej Obarski <mobarski@gmail.com>
2023-03-25 00:22:07 +0100

    agent reward/score WIP

--- ai_bricks/agent.py
+++ ai_bricks/agent.py
@@ -1,8 +1,11 @@
-from textwrap import dedent
 import re
 
+# TODO: reward
+# TODO: reflexion
 # TODO: pruning
 # TODO: loop detection
+
+REFLECTION_BAD_FORMAT = "Reflection: I didn't followed the format. I will try again."
 
 EXAMPLE = """
 Example:
@@ -12,14 +15,12 @@ Thought: I shoul look up the height of King Arthur first, then Queen Guinevere a
 Action: wikipedia-search
 Action Input: "King Arthur" height
 Observation: ['King Arthur']
+Reward: 0
 """
 EXAMPLE = ""
 
-def v1(question, model, actions, hints='', iter_limit=5):
-	out = {'text':''}
-	tmp = {'rtt_list':[], 'cost_list':[]}
-	print(f'\nQUESTION: {question}\n')
-	system = f"""
+def get_system_prompt(question, actions, hints):
+	return f"""
 Answer the following questions as best you can.
 Question: {question}
 
@@ -30,15 +31,25 @@ Thought: you should always think about what to do
 Action: the action to take, should be one of {' '.join(actions)}
 Action Input: the input to the action
 Observation: the result of the action
-... (this Thought/Action/Action Input/Observation can repeat N times)
+Score: observation score, 1 if progress was made, 0 otherwise
+... (this Thought/Action/Action Input/Observation/Reward can repeat N times)
 Thought: I now know the final answer
 Final Answer: the final answer to the original input question
 
 {EXAMPLE}
 """
-	#print('SYS PROMPT:', system) # XXX
-	prompt = """Begin!\n"""
+
+
+def v1(question, model, actions, hints='', iter_limit=5):
+	out = {'text':''}
+	tmp = {'rtt_list':[], 'cost_list':[]}
+	#
+	print(f'\nQUESTION: {question}\n')
+	system = get_system_prompt(question, actions, hints)
+	#print('SYSTEM PROMPT:', system) # XXX
+	prompt = "Begin!\n"
 	for i in range(iter_limit):
+		#print('PROMPT', prompt) # XXX
 		x = model.complete(prompt, system_prompt=system, stop=['Observation:'])
 		tmp['rtt_list'].append(x['rtt'])
 		tmp['cost_list'].append(x.get('cost',0))
@@ -47,11 +58,12 @@ Final Answer: the final answer to the original input question
 		#print('usage',x.get('usage',{}))
 		steps = re.findall('(?m)^([A-Z][^:]+):\s*(.*?)(?:\n|$)', x['text'])
 		for step in steps:
-			print(step[0]+':',step[1])
+			#print(step[0]+':',step[1])
 			pass
+		print(x['text'])
 		if not steps:
-			prompt = prompt + "Reflection: I didn't followed the format. I will try again.\n"
-			print("Reflection: I didn't followed the format. I will try again.") # XXX
+			prompt = prompt + REFLECTION_BAD_FORMAT + "\n"
+			print(REFLECTION_BAD_FORMAT) # XXX
 			continue
 		elif steps[-1][0] == 'Final Answer':
 			out['status'] = 'ok'
@@ -66,12 +78,12 @@ Final Answer: the final answer to the original input question
 				obs = f'Action "{action}" is not allowed. Allowed actions: {" ".join(actions)}.'
 			else:
 				obs = actions[action](input)
-			prompt = prompt + x['text'].rstrip() + f'\nObservation: {str(obs).rstrip()}\n'
+			prompt = prompt + x['text'].rstrip() + f'\nObservation: {str(obs).rstrip()}\n' #+'Reward:'
 			print(f"Observation: {str(obs).rstrip()}") # XXX
 			#print(prompt) # XXX
 		else:
-			prompt = prompt + "Reflection: I didn't followed the format. I will try again.\n"
-			print("Reflection: I didn't followed the format. I will try again.") # XXX
+			prompt = prompt + REFLECTION_BAD_FORMAT + "\n"
+			print(REFLECTION_BAD_FORMAT) # XXX
 			continue
 	else:
 		out['status'] = 'Error: iterations limit reached'
