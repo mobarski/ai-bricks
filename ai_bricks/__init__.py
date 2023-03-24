4ad840188041a95b9ee366219dfd7c9d5b90b02f
Maciej Obarski <mobarski@gmail.com>
2023-03-24 23:18:28 +0100

    api base class

---
+++ ai_bricks/__init__.py
@@ -0,0 +1,12 @@
+#import ai_bricks.api
+
+#def model(model_code, **kw):
+#	provider,_,name = model_code.partition(':')
+#	# TODO: refactor ???
+#	if provider=='openai':
+#		return api.openai.model(name, **kw)
+#	elif provider=='cohere':
+#		return api.cohere.model(name, **kw)
+#	elif provider=='anthropic':
+#		return api.anthropic.model(name, **kw)
+
